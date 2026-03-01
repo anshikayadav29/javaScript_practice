0b9473dfd2916f3b6f16c74699f759f1705be247
anshikayadav29 <anshikayadav2917@gmail.com>
2026-03-02 00:53:20 +0530

    Create Merge2array.js

---
+++ Merge2array.js
@@ -0,0 +1,14 @@
+// Merge Two Arrays
+let arr10a = [1, 3, 5];
+let arr10b = [2, 4, 6];
+let merged = [];
+
+for (let i = 0; i < arr10a.length; i++) {
+  merged.push(arr10a[i]);
+}
+
+for (let j = 0; j < arr10b.length; j++) {
+  merged.push(arr10b[j]);
+}
+
+console.log(merged);
