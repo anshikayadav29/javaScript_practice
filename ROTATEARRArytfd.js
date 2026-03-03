85019d2876041820b139e7c432828d45f0e1269a
anshikayadav29 <anshikayadav2917@gmail.com>
2026-03-03 14:24:50 +0530

    Create ROTATEARRArytfd.js

---
+++ ROTATEARRArytfd.js
@@ -0,0 +1,7 @@
+// Algorithm Name: Rotate Array
+function rotateArray(arr, k) {
+  k = k % arr.length;
+  return arr.slice(k).concat(arr.slice(0, k));
+}
+
+console.log(rotateArray([1,2,3,4,5], 2));
